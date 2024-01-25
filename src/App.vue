9a0bf436eb3880d8533d1c23e8f7553d19170c38
nik_nik <nikbox88@gmail.com>
2024-01-25 12:00:02 +0300

    header & main styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
+import LayoutHeader from "@/components/layout/Header.vue";
 </script>
 
-<template>
- 
+<template lang="pug">
+.container 
+  layout-header
+  .content   
+    router-view
 </template>
 
-<style scoped>
+<style lang="scss">
+@import "./styles/global.scss";
 
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
+  max-width: 1400px;
+  margin-left: 250px;
+  padding: 30px;
+  transition: 0.2s;
+  &_full {
+    margin-left: 0;
+  }
+}
 </style>
