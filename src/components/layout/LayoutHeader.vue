d2bf3e5d737f5e0a1240ace3e3cd4a1b3ed0de05
nik_nik <nikbox88@gmail.com>
2024-01-25 15:13:06 +0300

    layout header

---
+++ src/components/layout/LayoutHeader.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+header.header 
+    router-link(to="/") Ui Kit VCC 
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background: #fff;
+  font-weight: bold;
+  padding: 0 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.07);
+  height: 62px;
+  display: flex;
+  align-items: center;
+  z-index: 1;
+  position: relative;
+}
+</style>
