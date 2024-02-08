41d92a38ac07608e0f27e6d99498b6fa717bc47f
nik_nik <nikbox88@gmail.com>
2024-02-08 15:14:13 +0300

    setting for ts & pug

--- src/views/ButtonPage.vue
+++ src/views/ButtonPage.vue
@@ -1,14 +1,10 @@
-<script setup lang="ts">
-import Button from "@/components/Button.vue";
-import ClickableText from "@/components/ClickableText.vue";
-</script>
 
 <template lang="pug">
 h1.heading-1 Buttons  
 
 h1.heading-2 Colors    
 .line
-Button(label="Primary").item
+Button(label="Primary" ).item
 Button(label="Primary" disabled).item
 
 
@@ -20,8 +16,18 @@ Button(label="Outlined" outlined disabled).item
 
 h1.heading-2 Clickable text     
 .line Согласен с условиями &nbsp
-    ClickableText(text="правил использования сервиса и обработки персональных данных.").item
+  ClickableText(text="правил использования сервиса и обработки персональных данных.").item
 </template>
+<script setup lang="ts">
+import Button from "@/components/Button.vue";
+import ClickableText from "@/components/ClickableText.vue";
+
+// const toggleTheme = () => {
+//   let root = document.querySelector(":root");
+//   root?.classList.toggle('dark');
+
+// }
+</script>
 <style lang="scss">
 .item {
   margin-right: 10px;
